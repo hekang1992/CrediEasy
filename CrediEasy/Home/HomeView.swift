edf133ab71ec5d14201bd2f5f200a74297296e89
何康 <hekang19921111@hotmail.com>
2025-09-11 16:36:17 +0800

    add:新增apply

--- CrediEasy/Home/HomeView.swift
+++ CrediEasy/Home/HomeView.swift
@@ -24,6 +24,7 @@ class HomeView: BaseView {
     lazy var headImageView: UIImageView = {
         let headImageView = UIImageView()
         headImageView.image = UIImage(named: "home_head_image")
+        headImageView.isUserInteractionEnabled = true
         return headImageView
     }()
     
@@ -84,6 +85,75 @@ class HomeView: BaseView {
         return fourImageView
     }()
     
+    lazy var desclabel: UILabel = {
+        let desclabel = UILabel()
+        desclabel.text = "Maximum loan amount"
+        desclabel.textColor = UIColor.init(hexString: "#999B9C")
+        desclabel.textAlignment = .left
+        desclabel.font = UIFont.systemFont(ofSize: 14, weight: UIFont.Weight(400))
+        return desclabel
+    }()
+    
+    lazy var moneylabel: UILabel = {
+        let moneylabel = UILabel()
+        moneylabel.text = "₱120,000"
+        moneylabel.textColor = UIColor.init(hexString: "#1C2123")
+        moneylabel.textAlignment = .left
+        moneylabel.font = UIFont.systemFont(ofSize: 36, weight: UIFont.Weight(800))
+        return moneylabel
+    }()
+    
+    lazy var oneView: RateView = {
+        let oneView = RateView()
+        return oneView
+    }()
+    
+    lazy var twoView: RateView = {
+        let twoView = RateView()
+        return twoView
+    }()
+    
+    lazy var applyBtn: UIButton = {
+        let applyBtn = UIButton(type: .custom)
+        applyBtn.setTitle("Obtain loan amount", for: .normal)
+        applyBtn.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: UIFont.Weight(500))
+        applyBtn.setTitleColor(.white, for: .normal)
+        applyBtn.backgroundColor = UIColor.init(hexString: "#0073E5")
+        applyBtn.layer.cornerRadius = 27
+        applyBtn.layer.masksToBounds = true
+        return applyBtn
+    }()
+    
+    lazy var agreementLabel: UILabel = {
+        let agreementLabel = UILabel()
+        agreementLabel.numberOfLines = 0
+        let baseString = "I have read and agree to the loan terms."
+        let attributedString = NSMutableAttributedString(string: baseString)
+        let entireAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 12),
+            .foregroundColor: UIColor.init(hexString: "#001E3C")
+        ]
+        attributedString.addAttributes(entireAttributes, range: NSRange(location: 0, length: baseString.count))
+        let linkText = "loan terms"
+        if let linkRange = baseString.range(of: linkText) {
+            let nsRange = NSRange(linkRange, in: baseString)
+            attributedString.addAttributes([
+                .foregroundColor: UIColor.systemBlue,
+                .underlineStyle: NSUnderlineStyle.single.rawValue
+            ], range: nsRange)
+        }
+        agreementLabel.attributedText = attributedString
+        return agreementLabel
+    }()
+    
+    lazy var agreeBtn: UIButton = {
+        let agreeBtn = UIButton(type: .custom)
+        agreeBtn.isSelected = true
+        agreeBtn.setImage(UIImage(named: "login_nor_image"), for: .normal)
+        agreeBtn.setImage(UIImage(named: "login_sel_image"), for: .selected)
+        return agreeBtn
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(scrollView)
@@ -96,6 +166,16 @@ class HomeView: BaseView {
         minScrollView.addSubview(twoImageView)
         minScrollView.addSubview(threeImageView)
         minScrollView.addSubview(fourImageView)
+        
+        applyImageView.addSubview(desclabel)
+        applyImageView.addSubview(moneylabel)
+        applyImageView.addSubview(oneView)
+        applyImageView.addSubview(twoView)
+        applyImageView.addSubview(applyBtn)
+        
+        applyImageView.addSubview(agreementLabel)
+        applyImageView.addSubview(agreeBtn)
+        
         scrollView.snp.makeConstraints { make in
             make.top.left.right.equalToSuperview()
             make.bottom.equalTo(self.safeAreaLayoutGuide.snp.bottom)
@@ -147,6 +227,42 @@ class HomeView: BaseView {
             make.top.equalToSuperview()
             make.size.equalTo(CGSize(width: 143, height: 180))
             make.right.equalToSuperview().offset(-20)
+        }
+        desclabel.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(20)
+            make.top.equalToSuperview().offset(20)
+            make.height.equalTo(17)
+        }
+        moneylabel.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(20)
+            make.top.equalTo(desclabel.snp.bottom).offset(6)
+            make.height.equalTo(44)
+        }
+        oneView.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(20)
+            make.top.equalTo(moneylabel.snp.bottom).offset(12)
+            make.height.equalTo(26)
+            make.width.equalTo(110)
+        }
+        twoView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview().offset(20)
+            make.top.equalTo(moneylabel.snp.bottom).offset(12)
+            make.height.equalTo(26)
+        }
+        applyBtn.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(twoView.snp.bottom).offset(24)
+            make.size.equalTo(CGSize(width: 295, height: 54))
+        }
+        agreementLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview().offset(20)
+            make.top.equalTo(applyBtn.snp.bottom).offset(12)
+            make.height.equalTo(14)
+        }
+        agreeBtn.snp.makeConstraints { make in
+            make.size.equalTo(CGSize(width: 13, height: 13))
+            make.centerY.equalTo(agreementLabel.snp.centerY)
+            make.right.equalTo(agreementLabel.snp.left).offset(-5)
         }
     }
     
---
+++ CrediEasy/Home/RateView.swift
@@ -0,0 +1,48 @@
+//
+//  RateView.swift
+//  CrediEasy
+//
+//  Created by 何康 on 2025/9/11.
+//
+
+import UIKit
+
+class RateView: BaseView {
+    
+    lazy var logoImageView: UIImageView = {
+        let logoImageView = UIImageView()
+        logoImageView.image = UIImage(named: "rate_list_image")
+        logoImageView.contentMode = .scaleAspectFit
+        return logoImageView
+    }()
+    
+    lazy var desclabel: UILabel = {
+        let desclabel = UILabel()
+        desclabel.text = "91-240day"
+        desclabel.textColor = UIColor.init(hexString: "#1C2123")
+        desclabel.textAlignment = .left
+        desclabel.font = UIFont.systemFont(ofSize: 14, weight: UIFont.Weight(500))
+        return desclabel
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(logoImageView)
+        addSubview(desclabel)
+        logoImageView.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.size.equalTo(CGSize(width: 26, height: 26))
+        }
+        desclabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalTo(logoImageView.snp.right).offset(10)
+            make.height.equalTo(17)
+        }
+    }
+    
+    @MainActor required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
